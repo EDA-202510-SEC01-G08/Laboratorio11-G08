d10aab14e2df2ad21ba7afef28c479046add80c4
Archilipeper <p.archila@uniandes.edu.co>
2025-05-14 13:32:27 -0500

    queue arreglado

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -2,20 +2,20 @@ from DataStructures.List import array_list as ar
 from DataStructures.List import single_linked_list as sl
 
 def new_queue():
-    new_queue = sl.new_list()
+    new_queue = ar.new_list()
     return new_queue
 
 def enqueue(my_queue, element):
-    return sl.add_last(my_queue, element)
+    return ar.add_last(my_queue, element)
 
 def dequeue(my_queue):
-    return sl.remove_first(my_queue)
+    return ar.remove_first(my_queue)
 
 def is_empty(my_queue):
-    return sl.is_empty(my_queue)
+    return ar.is_empty(my_queue)
     
 def peek(my_queue):
-    return sl.first_element(my_queue)
+    return ar.first_element(my_queue)
 
 def size(my_queue):
-    return sl.size(my_queue)
+    return ar.size(my_queue)
