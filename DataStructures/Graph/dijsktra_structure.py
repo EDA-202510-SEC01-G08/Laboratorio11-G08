9e72d542ffb7cd9a009abd23d2d20dd0d89bd7a2
juanccancelado <j.canceladod@uniandes.edu.co>
2025-05-22 22:52:33 -0500

    cambios en pq, dijkstra, digraph para dijkstra perfecto

--- DataStructures/Graph/dijsktra_structure.py
+++ DataStructures/Graph/dijsktra_structure.py
@@ -1,5 +1,6 @@
 from DataStructures.Map import map_linear_probing as mp
 from DataStructures.Priority_queue import priority_queue as pq
+from DataStructures.Graph import digraph as gr
 
 
 def new_dijsktra_structure(source, g_order):
@@ -20,47 +21,69 @@ def new_dijsktra_structure(source, g_order):
         "source": source,
         "visited": mp.new_map(
             g_order, 0.5),
-        "pq": pq.new_heap(),
+        "pq": pq.new_heap(comparison),
         "predecessors": {}
         }
     return structure
 
+def comparison(a, b):
+    # a and b are tuples: (distance, vertex)
+    if a[0] == b[0]:
+        return 0
+    elif a[0] < b[0]:
+        return 1  # menor distancia = mayor prioridad
+    else:
+        return -1
+
 def dijkstra(my_graph, source):
     
-    if my_graph["order"] == 0:
+    if gr.order(my_graph) == 0:
         raise Exception("El grafo está vacío, no se puede ejecutar Dijkstra.")
     else:
-        aux_structure = new_dijsktra_structure(source, my_graph["order"])
-        pq.insert(aux_structure["pq"], source, 0)
-        mp.put(aux_structure["visited"], source, 0)
+        aux_structure = new_dijsktra_structure(source, gr.order(my_graph))
+        pq.insert(aux_structure["pq"], (0, source, None))  # None porque el source no tiene predecesor
+
         while not pq.is_empty(aux_structure["pq"]):
-            vertex = pq.remove(aux_structure["pq"])
-            adjacents = my_graph["vertices"][vertex]["adjacents"]
-            for i in adjacents:
-                if i is not None and i["key"] not in aux_structure["visited"]["elements"]:
-                    new_distance = i["weight"] + dist_to(vertex, aux_structure)
-                    if new_distance < dist_to(i["key"], aux_structure):
-                        pq.insert(aux_structure["pq"], i["key"], new_distance)
-                        mp.put(aux_structure["visited"], i["key"], new_distance)
-                        aux_structure["predecessor"][i["key"]] = vertex  
+
+            current_distance, vertex, predecessor = pq.remove(aux_structure["pq"])
+
+            if mp.get(aux_structure["visited"], vertex) is not None:
+                continue
+            mp.put(aux_structure["visited"], vertex, current_distance)
+            if predecessor is not None:
+                aux_structure["predecessors"][vertex] = predecessor
+            adjacents = mp.value_set(gr.adjacents(my_graph, vertex))
+            for adj in adjacents["elements"]:
+                neighbor = adj["to"]
+                weight = adj["weight"]
+                if mp.get(aux_structure["visited"], neighbor) is None:
+                    new_distance = current_distance + weight
+                    pq.insert(aux_structure["pq"], (new_distance, neighbor, vertex))
         return aux_structure
     
 def dist_to(key_v, aux_structure):
-    
-    if key_v in aux_structure["visited"]["elements"]:
-        return aux_structure["visited"]["elements"][key_v]
+    # Use the map's get function to retrieve the distance for key_v
+    distance = mp.get(aux_structure["visited"], key_v)
+    if distance is not None:
+        return distance
     else:
         raise Exception("No existe una distancia registrada para el vértice dado.")
     
 def has_path_to(key_v, aux_structure):
-
-    if key_v in aux_structure["visited"]["elements"]:
+    """
+    Retorna True si existe un camino desde el vértice fuente hasta key_v,
+    es decir, si key_v fue visitado por Dijkstra.
+    """
+    if mp.get(aux_structure["visited"], key_v) is not None:
         return True
     else:
-        raise Exception("No existe un camino desde el vértice fuente hasta el vértice dado.")
-
-def path_to(key_v, aux_structure):
+        return False
     
+def path_to(key_v, aux_structure):
+    """
+    Retorna la lista de vértices que conforman el camino más corto desde el vértice fuente
+    hasta key_v, en orden desde el origen hasta el destino.
+    """
     if not has_path_to(key_v, aux_structure):
         raise Exception("No existe un camino desde el vértice fuente hasta el vértice dado.")
     
@@ -68,7 +91,7 @@ def path_to(key_v, aux_structure):
     current = key_v
     while current != aux_structure["source"]:
         path.append(current)
-        current = aux_structure["predecessor"][current]
+        current = aux_structure["predecessors"][current]  # Usa el nombre correcto del diccionario
     path.append(aux_structure["source"])
-    path.reverse()  
+    path.reverse()
     return path
